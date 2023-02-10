8606379fd7da29373c6396ec97c798b92a936c8a
Tartachocol <flcara278@ieszaidinvergeles.org>
2023-02-10 10:28:40 +0100

    Areglo login, Register,

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -3,67 +3,70 @@ import 'package:proyecto_peluqueria_fjjm/widgets/widgets.dart';
 import 'package:proyecto_peluqueria_fjjm/routes/routes.dart';
 
 class LoginScreen extends StatelessWidget {
-   
   const LoginScreen({Key? key}) : super(key: key);
-  
+
   @override
   Widget build(BuildContext context) {
-    
     final myFormKey = GlobalKey<FormState>();
 
-    final Map<String, String> formValues = {
-      'email': '',
-      'password': ''
-    };
+    final Map<String, String> formValues = {'email': '', 'password': ''};
 
     return Scaffold(
       body: Form(
         key: myFormKey,
-          child: SingleChildScrollView(
-            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-            child: Column(
-              children: [
-                const SizedBox(height: 50,),
-                const FlutterLogo(size: 100),
-                const SizedBox(height: 30,),
-                CustomTextFormField(
-                  hintText: 'Ejemplo@ejemplo.com',
-                  suffixIcon: Icons.group,
-                  formProperty: 'email',
-                  formValues: formValues,
-                ),
-                const SizedBox(height: 30,),
-                CustomTextFormField(
-                  hintText: 'Contraseña',
-                  suffixIcon: Icons.password_outlined,
-                  obscureText: true,
-                  formProperty: 'password',
-                  formValues: formValues,
-                ),
-                const SizedBox(height: 30,),
-                ElevatedButton(
-                  onPressed: () {
-                    FocusScope.of(context).requestFocus(FocusNode());
-                    if( !myFormKey.currentState!.validate()) {
-                      print('Formulario no válido');
-                      return;
-                    } else {
-                      print(formValues);
-                    }
-                  }, 
-                  child: const SizedBox(
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 50,
+              ),
+              const FlutterLogo(size: 100),
+              const SizedBox(
+                height: 30,
+              ),
+              CustomTextFormField(
+                hintText: 'Email',
+                suffixIcon: Icons.group,
+                formProperty: 'email',
+                formValues: formValues,
+                keyboardType: TextInputType.emailAddress,
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              CustomTextFormField(
+                hintText: 'Contraseña',
+                suffixIcon: Icons.password_outlined,
+                obscureText: true,
+                formProperty: 'password',
+                formValues: formValues,
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  FocusScope.of(context).requestFocus(FocusNode());
+                  if (!myFormKey.currentState!.validate()) {
+                    print('Formulario no válido');
+                    return;
+                  } else {
+                    print(formValues);
+                  }
+                },
+                child: const SizedBox(
                     width: double.infinity,
-                    child: Center(child: Text('Iniciar Sesión'))
-                  ),
-                ),
-              ],
-            ),
+                    child: Center(child: Text('Iniciar Sesión'))),
+              ),
+            ],
           ),
+        ),
       ),
       floatingActionButton: FloatingActionButton(
-                  onPressed: () => Navigator.pop(context),
-                  child: const Icon(Icons.close),
-                ),
+        onPressed: () => Navigator.pop(context),
+        child: const Icon(Icons.close),
+      ),
     );
   }
 }
--- lib/widgets/custom_text_form_field.dart
+++ lib/widgets/custom_text_form_field.dart
@@ -1,25 +1,24 @@
 import 'package:flutter/material.dart';
 
 class CustomTextFormField extends StatelessWidget {
-
   final String? hintText;
   final String? helperText;
   final IconData? suffixIcon;
 
   final bool obscureText;
-
+  final TextInputType? keyboardType;
   final String formProperty;
   final Map<String, String> formValues;
 
-
   const CustomTextFormField({
-    super.key, 
-    this.hintText, 
-    this.helperText, 
+    super.key,
+    this.hintText,
+    this.helperText,
     this.suffixIcon,
-    required this.formProperty, 
-    required this.formValues, 
-    this.obscureText =false,
+    required this.formProperty,
+    required this.formValues,
+    this.obscureText = false,
+    this.keyboardType,
   });
 
   @override
@@ -28,17 +27,22 @@ class CustomTextFormField extends StatelessWidget {
       autofocus: true,
       onChanged: (value) => formValues[formProperty] = value,
       obscureText: obscureText,
+      keyboardType: keyboardType,
       validator: (value) {
-        if(formValues['email'] == value){
-          if( !formValues['email']!.contains("@")){
-            return 'El email no es válido';
+        if (hintText == 'Email') {
+          if (formValues['email'] == value) {
+            if (!formValues['email']!.contains("@")) {
+              return 'El email no es válido';
+            }
           }
-        }
-        
-        if(formValues['password'] == value) {
-          if( formValues['password']!.length == 0){
-            return 'La contraseña no es válida';
-          } 
+        } else {
+          if (hintText == 'password') {
+            if (formValues['password'] == value) {
+              if (formValues['password']!.length == 0) {
+                return 'La contraseña no es válida';
+              }
+            }
+          }
         }
       },
       autovalidateMode: AutovalidateMode.onUserInteraction,
