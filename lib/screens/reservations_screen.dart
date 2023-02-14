3d632b6ce1fc2788a977ab9be0654fa4225b916c
manulopez14 <mlopez14@ieszaidinvergeles.org>
2023-02-14 12:58:20 +0100

    Cambios en reservas, alerta
    Se han realizado cambios en el screen de reservas y se ha añadido una alerta para la cancelación de reservas.

--- lib/screens/reservations_screen.dart
+++ lib/screens/reservations_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:proyecto_peluqueria_fjjm/widgets/widgets.dart';
 
 class ReservationsScreen extends StatefulWidget {
   const ReservationsScreen({Key? key}) : super(key: key);
@@ -17,12 +18,49 @@ class _ReservationsScreenState extends State<ReservationsScreen> {
 
 ];
 */
+
+  void displayDialog(BuildContext context){
+    showDialog(
+      context: context, 
+      builder: ( context ) {
+        return AlertDialog(
+          title: const Text("Cancelación de reserva"),
+          content: const Text("¿Estás seguro de que quieres cancelar tu reserva del día X a las XX:XX en el local X?"),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () => Navigator.pop(context), 
+              child: const Text("Cancelar")
+            ),
+            TextButton(
+              onPressed: () => Navigator.pop(context), 
+              child: const Text("Confirmar")
+            ),
+          ],
+        );
+      }
+    );
+  }
+
   List<bool> _isOpen = [false, false];
 
   @override
   Widget build(BuildContext context) {
     _isOpen.toList(growable: true);
     return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: const Text('Flutter App'),
+        actions: const [
+          Padding(
+            padding: EdgeInsets.only(right: 10.0),
+            child: CircleAvatar(
+                maxRadius: 20,
+                backgroundImage: NetworkImage('https://img.europapress.es/fotoweb/fotonoticia_20220720184850_420.jpg'),
+            ),
+          ),
+        ],
+      ),
+      bottomNavigationBar: buttonNavigationBar(),
       body: SingleChildScrollView(
         padding: const EdgeInsets.all(20),
         child: ExpansionPanelList(
@@ -31,28 +69,37 @@ class _ReservationsScreenState extends State<ReservationsScreen> {
           dividerColor: Colors.green,
           elevation: 1,
           children: [
-            ExpansionPanel(
-              canTapOnHeader: true,
-              isExpanded: _isOpen[0],
-              headerBuilder: (context, isOpen) {
-                return const ListTile(
-                  title: Text('Reserva 1'),
-                );
-              },
-              body: ListTile(
-                title: Text('TextoA'),
-                
-                trailing: const Icon(Icons.delete),
-                onTap: () {
-                  setState(() {});
-                },
-              ),
-            ),
+            _Reserva(context),
           ],
           expansionCallback: (i, isOpen) =>
               setState(() => _isOpen[i] = !isOpen),
         ),
       ),
     );
+  }
+
+  ExpansionPanel _Reserva(BuildContext context) {
+    return ExpansionPanel(
+            canTapOnHeader: true,
+            isExpanded: _isOpen[0],
+            headerBuilder: (context, isOpen) {
+              return const ListTile(
+                title: Text('Reserva 1'),
+              );
+            },
+            body: ListTile(
+              title: Text('TextoA'),
+              
+              trailing: GestureDetector(
+                child: Text("Cancelar"),
+                onTap: () {
+                  displayDialog(context);
+                },
+              ),
+              onTap: () {
+                setState(() {});
+              },
+            ),
+          );
   }
 }
